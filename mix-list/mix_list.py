0f69928dc3556f6f1b1577e84d60ac7a40a6bbb4
cesarpevezfajardo-telus <cesar.pevezfajardo@telus.com>
2019-10-01 12:02:42 -0400

    update 2

---
+++ mix-list/mix_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+my_list = [ "192.168.128.1", 5060, "UP" ]
+print ("the first item in my list is: " + my_list[0])
+print ("the second item in my list is: " + str(my_list[1]))
+print ("the third item in the list is: " + my_list[2])
+
